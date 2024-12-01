0d6280f84437b159625586998c3e2ab65ff2c7a6
rishabroyc <rroycho2@uwo.ca>
2024-12-01 13:14:35 -0500

    setup my connection

--- SRC/backend/config/db.js
+++ SRC/backend/config/db.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
-  user: 'sqluser',
+  user: 'sqluser1',
   password: 'password',
   database: 'the_box_office'
 });
